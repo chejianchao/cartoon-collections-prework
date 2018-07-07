ba8161cdbc20b64c0087499d7493d940c3b4f7ac
chejianchao <820992376@qq.com>
2018-07-07 01:16:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,11 @@ def roll_call_dwarves(array)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
-  
+  array.map {|item|
+    item[0] = item[0].upcase
+  }
 end
 
 def long_planeteer_calls# code an argument here
